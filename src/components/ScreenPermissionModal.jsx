de4f241227397ee751f02f86784a723a201c85c9
Karthick <karthickm0803@gmail.com>
2026-05-22 15:07:50 +0530

    screen record

--- src/components/ScreenPermissionModal.jsx
+++ src/components/ScreenPermissionModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { useScreenMonitor } from './ScreenMonitorContext';
+import { useScreenMonitor } from '../context/ScreenMonitorContext';
 import './ScreenPermissionModal.css';
 
 export default function ScreenPermissionModal() {
